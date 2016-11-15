367ea6f446d74005a5fc6c7a28a131b899e4c11e
diwaker_surya <diwaker.singh@games24x7.com>
2016-11-15 12:51:56 +0530

    removed hmre for production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
 				exclude: /node_modules/,
 				loader: 'babel',
 				query: {
-					presets: ['react', 'es2015', 'react-hmre']
+					presets: ['react', 'es2015']
 				}
 			}, {
 				test: /\.css$/,
