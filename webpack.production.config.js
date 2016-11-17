72d67100de7dc3b9e440c6301dd896e8f8dd8a6b
diwaker_surya <diwaker.singh@games24x7.com>
2016-11-17 14:27:24 +0530

    modified production webpack file for font awsome

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,6 +17,21 @@ module.exports = {
 			}, {
 				test: /\.css$/,
 				loader: "style-loader!css-loader"
+			}, {
+				test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/font-woff"
+			}, {
+				test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/font-woff"
+			}, {
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/octet-stream"
+			}, {
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file"
+			}, {
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=image/svg+xml"
 			}
 		]
 	},
