bea5ae2b7fc6d091d7b400aad6788620ecf9e1d2
Diwaker Singh <diwaker.singh@inmobi.com>
2019-08-21 00:38:56 +0530

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import route from "./utils/routeConfig";
 import Feature from './containers/Feature';
-ReactDOM.render(
-	<Feature route={route}/>, document.querySelector('.container'))
+ReactDOM.render(<Feature />, document.querySelector('.container'));
--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Match from 'react-router/Match';
-export const MatchWithSubRoutes = (route) => (
-	<Match {...route} render={(props) => (<route.component {...props} subRoutes={route.subRoutes}/>)}/>
-)
