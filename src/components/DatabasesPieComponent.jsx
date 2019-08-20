9b2e1f6c5696b070cfe8413564b45d26e2926c9a
Diwaker Singh <diwaker.singh@inmobi.com>
2019-08-21 00:11:59 +0530

    test

--- src/components/DatabasesPieComponent.jsx
+++ src/components/DatabasesPieComponent.jsx
@@ -1,20 +1,20 @@
-import React from "react"
-let width = 400,
-  height = 300
-const name = d => d.name
-const value = d => d.value
-const chartSeries = []
+import React from 'react';
+let width = 500,
+    height = 300;
+const name = d => d.name;
+const value = d => d.value;
+const chartSeries = [];
 const DatabasesPie = ({ databases }) => {
-  //creating the seried for the chart based on the data.
-  databases.forEach(database =>
-    chartSeries.push({
-      field: database.name,
-      name: database.name,
-    })
-  )
-  return (
-    <div className="mdl-card__supporting-text">
-      {/* <PieChart
+    //creating the seried for the chart based on the data.
+    databases.forEach(database =>
+        chartSeries.push({
+            field: database.name,
+            name: database.name
+        })
+    );
+    return (
+        <div className="mdl-card__supporting-text">
+            {/* <PieChart
         id={"databases"}
         data={databases}
         width={width}
@@ -23,7 +23,7 @@ const DatabasesPie = ({ databases }) => {
         value={value}
         name={name}
       /> */}
-    </div>
-  )
-}
-export default DatabasesPie
+        </div>
+    );
+};
+export default DatabasesPie;
